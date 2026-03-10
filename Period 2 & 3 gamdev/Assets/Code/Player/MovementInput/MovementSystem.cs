a5947808a9011aa5acfc95130bbc7f0e03bf241a
Lm-Pjunior <julian.vlaming@student.rocvf.nl>
2026-03-10 10:53:04 +0100

    Added unfinished movement system
    This movement system still some work but the flip function has been added :D

--- Period 2 & 3 gamdev/Assets/Code/Player/MovementInput/MovementSystem.cs
+++ Period 2 & 3 gamdev/Assets/Code/Player/MovementInput/MovementSystem.cs
@@ -1,12 +1,32 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MovementSystem : MonoBehaviour
 {
+    [SerializeField] private float _speed = 20f;
+    [SerializeField] private float _jumpForce = 26f;
+
+    private bool _grounded;
+    private Rigidbody2D _body;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Awaken()
     {
-        
+       _body = GetComponent<Rigidbody2D>();
     }
+
+    public void LMovement()
+    {
+        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
+    public void RMovement()
+    {
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+    }
+    // it's something like this: transform.position += (new Vector3(0,1,0); * Time.deltaTime * _speed)
+    // if(Input.GetKey(KeyCode.A)){ body.linearVelocityX = -5; }
 
     // Update is called once per frame
     void Update()
