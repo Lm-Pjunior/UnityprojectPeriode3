c9c65d5017699e2d4ba45199e941feb565ae5cc6
Lm-Pjunior <julian.vlaming@student.rocvf.nl>
2026-04-10 10:49:21 +0200

    Added a new collision script
    this doesn't quite fit the feature description but ehhhhh..... t'is whut it is.
    
    (it's a start btw the collision bit itself is not there)

---
+++ Period 2 & 3 gamdev/Assets/Code/Player/Collision/OnCollision.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class OnCollision : MonoBehaviour
+{
+
+
+
+    [SerializeField] private GameObject _GameObjectName;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
